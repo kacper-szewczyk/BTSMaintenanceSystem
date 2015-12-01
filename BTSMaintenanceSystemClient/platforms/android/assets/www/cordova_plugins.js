441fa79ae9005896e37efcf2d19606f0336be283
Slyboots92 <michal.blach92@gmail.com>
2015-12-01 03:22:55 +0100

    Intro view and go forward done

---
+++ BTSMaintenanceSystemClient/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,39 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "id": "cordova-plugin-dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.0",
+    "cordova-plugin-console": "1.0.2",
+    "org.apache.cordova.device": "0.3.0",
+    "cordova-plugin-dialogs": "1.2.0"
+}
+// BOTTOM OF METADATA
+});
---
+++ BTSMaintenanceSystemClient/platforms/android/assets/www/js/intro.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+
+function callAnothePage() {
+	console.log("callAnothePage");
+	window.location = "html/intro.html";
+}
