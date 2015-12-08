22e49915365efbe6261f956611c1094764787a96
mistrzszefow <kacper.szewczyk@wp.pl>
2015-12-09 00:22:47 +0100

    Updating header in search added

--- BTSMaintenanceSystemClient/www/js/search.js
+++ BTSMaintenanceSystemClient/www/js/search.js
@@ -6,3 +6,11 @@ function callAnotherPage() {
     console.log("callAnotherPage");
     window.location = "html/search.html";
 }
+
+function updateHeader(url) {
+    var website = "search.html";
+    var index = url.indexOf(website);
+
+    return url.substring(index+website.length,url.length);
+}
+//myObject = updateHeader.call(myObject, "search.html#12");     // Will return 20
