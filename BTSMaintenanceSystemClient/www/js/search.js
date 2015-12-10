e63863864bd6f8362caded14725c6f3f4269e509
mistrzszefow <kacper.szewczyk@wp.pl>
2015-12-10 01:11:23 +0100

    search updated

--- BTSMaintenanceSystemClient/www/js/search.js
+++ BTSMaintenanceSystemClient/www/js/search.js
@@ -1,6 +1,45 @@
 /**
  *
  */
+$(document).ready(function() {
+    var SearchModule = (function () {
+
+        var state = 0,
+            $navigation;
+        return {
+            init: function () {
+                $navigation = $('#popupMenuHandler');
+                var self = this;
+                console.log($navigation);
+                $navigation.click(function () {
+                    alert('sdfsdf');
+                    self.openHamburger();
+
+                });
+
+                $('.filterElements a').click(function() {
+                    var type = $(this).data('type');
+                    console.log($(this));
+                    self.chooseNavElement(type)
+                });
+
+            },
+            openHamburger: function () {
+                state = 1;
+                alert('test');
+            },
+            chooseNavElement : function(type) {
+                if (type === null) {
+                    return;
+                }
+                alert('Clicked on element '+ type );
+            }
+        };
+    })();
+    console.log(SearchModule);
+    SearchModule.init();
+});
+
 
 function callAnotherPage() {
     console.log("callAnotherPage");
@@ -11,6 +50,6 @@ function updateHeader(url) {
     var website = "search.html";
     var index = url.indexOf(website);
 
-    return url.substring(index+website.length,url.length);
+    return url.substring(index + website.length, url.length);
 }
 //myObject = updateHeader.call(myObject, "search.html#12");     // Will return 20
