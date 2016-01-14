771dfa60d0790066979f7e8e5ecf3d71dbab83f4
Kacper Szewczyk <kacpersz@dublin.ad.s3group.com>
2016-01-14 13:39:43 +0100

    Sms first try

--- BTSMaintenanceSystemClient/www/js/sms.js
+++ BTSMaintenanceSystemClient/www/js/sms.js
@@ -4,18 +4,51 @@ $(document).ready(function() {
         var state = 0;
         return {
             init: function () {
-                document.getElementById("telephoneNumber").innerHTML = "505 125 640";
+                document.getElementById("telephoneNumber").innerHTML = "693948562";
 
                 $("#editNumberButton").click(function(){
-                    alert("Nie zmienisz numeru kosika");
+                    alert("Nie zmienisz numeru kosika ");
+
                 });
 
                 $("#lookUpButton").click(function(){
                     alert("Nie wiem co to miało robić");
+                    var telephoneNumber = document.getElementById('telephoneNumber').innerHTML;
+                    telephoneNumber = reduceWhitespaces(telephoneNumber);
+                    sendSms(telephoneNumber,"No jarosław");
                 });
-
+                
+                $("#typeOfMessageSelect").change(function () {
+                    $( "#typeOfMessageSelect option:selected").each(function() {
+                        alert($(this).text());
+                    });
+                });
             }
         };
     })();
     SearchModule.init();
 });
+
+function reduceWhitespaces(number) {
+    var oldNumber = "111";
+    while(oldNumber.localeCompare(number) != 0){
+        oldNumber = number;
+        number = oldNumber.replace(" ","");
+    }
+    return number;
+}
+
+function sendSms(number, message) {
+    //CONFIGURATION
+    var options = {
+        replaceLineBreaks: false, // true to replace \n by a new line, false by default
+        android: {
+            intent: 'INTENT'  // send SMS with the native android SMS messaging
+            //intent: '' // send SMS without open any other app
+        }
+    };
+
+    var success = function () { alert('Message sent successfully'); };
+    var error = function (e) { alert('Message Failed:' + e); };
+    sms.send(number, message, options, success, error);
+}
